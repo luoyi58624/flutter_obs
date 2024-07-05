53da4a59f18402a27ee84e1ca89fa67a5802108e
luoyi <luoyi58624@163.com>
2024-07-05 15:11:55 +0800

    chore: ObsBuilder变更为StatefulWidget

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -1,6 +1,6 @@
 part of '../flutter_obs.dart';
 
-class ObsBuilder extends StatelessWidget {
+class ObsBuilder extends StatefulWidget {
   /// 响应式变量构建器，监听内部的响应式变量，当变量发生变更时，将重建小部件
   const ObsBuilder({super.key, required this.builder, this.binding});
 
@@ -11,30 +11,18 @@ class ObsBuilder extends StatelessWidget {
   final List<Obs>? binding;
 
   @override
-  StatelessElement createElement() {
-    print('xx');
-    return _Element(this, binding);
-  }
-
-  @override
-  Widget build(BuildContext context) => builder(context);
+  State<ObsBuilder> createState() => _ObsBuilderState();
 }
 
-class _Element extends StatelessElement {
-  _Element(super.widget, this.binding);
-
-  final List<Obs>? binding;
-
+class _ObsBuilderState extends State<ObsBuilder> {
   /// 保存销毁[Obs]变量的监听函数集合，一个构建器可以存在多个响应式变量，该组件被销毁时，
   /// 需要通知所有的响应式变量移除此构建器的更新函数
   final Set<VoidCallback> removeNotifyFunList = {};
 
-  /// 拦截小部件构建的生命周期，为响应式变量建立关联
   @override
-  Widget build() {
-    if (binding != null && binding!.isNotEmpty) {
-      print(binding);
-      for (final obs in binding!) {
+  Widget build(BuildContext context) {
+    if (widget.binding != null && widget.binding!.isNotEmpty) {
+      for (final obs in widget.binding!) {
         if (!obs._notifyFunList.contains(_notify)) {
           obs.addListener(_notify);
           removeNotifyFunList.add(() => obs.removeListener(_notify));
@@ -42,27 +30,26 @@ class _Element extends StatelessElement {
       }
     }
     _notifyFun = _notify;
-    var result = super.build();
+    var result = widget.builder(context);
     _notifyFun = null;
     removeNotifyFunList.addAll(_removeNotifyFunList);
     _removeNotifyFunList.clear();
     return result;
   }
 
-  /// 卸载时移除监听
   @override
-  void unmount() {
+  void dispose() {
     for (var fun in removeNotifyFunList) {
       fun();
     }
     removeNotifyFunList.clear();
-    super.unmount();
+    super.dispose();
   }
 
   /// 更新函数，响应式变量发生变更就是执行它们让页面刷新
   void _notify() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (mounted) markNeedsBuild();
+      if (mounted) setState(() {});
     });
   }
 }
